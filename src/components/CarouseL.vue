fc0c61f9edbc6108fe6f0dbb2bb633ba85a3fd68
idanfath <arfounded@gmail.com>
2023-11-23 11:12:06 +0800

    a

--- src/components/CarouseL.vue
+++ src/components/CarouseL.vue
@@ -9,22 +9,22 @@
       height="300px"
       class="bg-transparent text-black rounded-borders"
     >
-      <q-carousel-slide name="1" class="column no-wrap flex-center">
+      <q-carousel-slide :name="1" class="column no-wrap flex-center">
         <img style="width: 100px;" src="https://smktelkom-mks.sch.id/assets/front-end/img/icon/school_jfslqk.png">
         <div class="q-mt-md text-center text-bold" style="font-size: 20px;">Lingkungan Nyaman</div>
         <div class="q-mt-sm text-center" style="font-size: 15px;">Berada di lingkungan yang asri, aman dan kondusif.</div>
       </q-carousel-slide>
-      <q-carousel-slide name="2" class="column no-wrap flex-center">
+      <q-carousel-slide :name="2" class="column no-wrap flex-center">
         <img style="width: 100px;" src="https://smktelkom-mks.sch.id/assets/front-end/img/icon/presentation_dai7nn.png">
         <div class="q-mt-md text-center text-bold" style="font-size: 20px;">Pengajar Kompeten</div>
         <div class="q-mt-sm text-center" style="font-size: 15px;">Lorem ipsum, dolor sit amet.</div>
       </q-carousel-slide>
-      <q-carousel-slide name="3" class="column no-wrap flex-center">
+      <q-carousel-slide :name="3" class="column no-wrap flex-center">
         <img style="width: 100px;" src="https://smktelkom-mks.sch.id/assets/front-end/img/icon/team_lxvtyk.png">
         <div class="q-mt-md text-center text-bold" style="font-size: 20px;">Kerjasama Luas</div>
         <div class="q-mt-sm text-center" style="font-size: 15px;">Lorem ipsum, dolor sit amet.</div>
       </q-carousel-slide>
-      <q-carousel-slide name="4" class="column no-wrap flex-center">
+      <q-carousel-slide :name="4" class="column no-wrap flex-center">
         <img style="width: 100px;" src="https://smktelkom-mks.sch.id/assets/front-end/img/icon/laptop_ksosxp.png">
         <div class="q-mt-md text-center text-bold" style="font-size: 20px;">Fasilitas Lengkap</div>
         <div class="q-mt-sm text-center" style="font-size: 15px;">Lorem ipsum, dolor sit amet.</div>
@@ -39,7 +39,7 @@
   export default {
     setup () {
       return {
-        slide: ref('style'),
+        slide: ref(1),
         autoplay: ref(true)
       }
     }
